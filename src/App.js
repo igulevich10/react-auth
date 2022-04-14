772cd3b2c46d9b83471299aeebc9eb2018f00c94
igulevich10 <78545961+igulevich10@users.noreply.github.com>
2022-04-14 13:21:03 +0300

    update react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import SignUp from "./components/SignUp";
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdateProfile from "./components/UpdateProfile";
+import Start from "./components/Start";
+import { AuthProvider } from "./contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./routes/PrivateRoute"; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Container 
+      className="d-flex align-items-center justify-content-center" 
+      style={{ minHeight: "100vh" }}
+    >
+      <div className="w-100" style={{maxWidth: "400px"}}>
+        <Router>
+          <AuthProvider>
+            <Routes>
+              <Route path='/' element={<Start/>} />
+              <Route path='/dashboard' element={<PrivateRoute>
+                <Dashboard/>
+              </PrivateRoute>} />
+              <Route path='/update' element={<PrivateRoute>
+                <UpdateProfile/>
+              </PrivateRoute>} />
+              <Route path='/signup' element={<SignUp/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/forgot-password' element={<ForgotPassword/>} />
+              
+            </Routes>
+          </AuthProvider>
+        </Router>
+      </div>
+    </Container>   
+    </>
+  )
 }
 
 export default App;
